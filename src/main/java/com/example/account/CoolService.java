50081460d0ac738f83f04ac8a3211fa4c9669ede
xiangming.song <sermitulla@gmail.com>
2020-01-30 12:52:03 +0800

    add tostring

--- src/main/java/com/example/account/CoolService.java
+++ src/main/java/com/example/account/CoolService.java
@@ -43,6 +43,10 @@ public class CoolService {
 
     }
 
-
-
+    @Override
+    public String toString() {
+        return "CoolService{" +
+                "someString='" + someString + '\'' +
+                '}';
+    }
 }
